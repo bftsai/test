78a1aeeca074d9bf0df507a1917b12d50a17ec15
bf_tsai <tsai19911026@gmail.com>
2023-09-14 22:30:46 +0800

    adjust

--- main.js
+++ main.js
@@ -1,3 +1,20 @@
 import './assets/scss/all.scss';
 
-console.log("Hello world!");
+$(document).ready(function () {
+    $('.item1').click(function (e) { 
+        e.preventDefault();
+        $('.mainPic').css("background","url('../images/shelter/1/1.png') center center no-wrap").css("background-size","cover");
+    });
+    $('.item2').click(function (e) { 
+        e.preventDefault();
+        $('.mainPic').css("background-image","url(../images/shelter/1/2.png)").css("background-position","center").css("background-repeat","no-repeat").css("background-size","cover");
+    });
+    $('.item3').click(function (e) { 
+        e.preventDefault();
+        $('.mainPic').css("background","url(../images/shelter/1/3.png) center center no-wrap").css("background-size","cover");
+    });
+    $('.item4').click(function (e) { 
+        e.preventDefault();
+        $('.mainPic').css("background","url(../images/shelter/1/4.jpg) center center no-wrap").css("background-size","cover");
+    });
+});
